d1f939fde42f62f9a270f9dd1614bce04d57a9c0
Shraddha Barke <shraddha.6596@gmail.com>
2020-05-02 00:21:42 -0700

    cvc4 size computation

--- src/test/benchmarks/cvc4-syguscomp.sh
+++ src/test/benchmarks/cvc4-syguscomp.sh
@@ -3,10 +3,8 @@
 for f in euphony/*.sl; do
 	STARTTIME=$(date +%s%N)
 	out=`timeout 600 /c/users/shrad/downloads/cvc4-1.7-win64-opt.exe $f`
-	retval=$?
 	ENDTIME=$(date +%s%N)
-	echo "$f,$retval,$((($ENDTIME - $STARTTIME)/1000000000))"
-	if [ $retval = 0 ]; then
-		echo $out | tr '\r' '\n' | awk 'END{print}'
-	fi
+	ret=$(echo $out | tr '\r' '\n' | awk 'END{print}')
+	strret=( $ret )
+	echo ${#strret[@]},$((($ENDTIME - $STARTTIME)/1000000000)),$ret
 done
