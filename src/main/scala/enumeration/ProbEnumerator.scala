b0915c1ffbde894788565f51751343011a692c07
Shraddha Barke <shraddha.6596@gmail.com>
2020-05-14 01:03:04 -0700

    fixed a bug with major consequences

--- src/main/scala/enumeration/ProbEnumerator.scala
+++ src/main/scala/enumeration/ProbEnumerator.scala
@@ -34,7 +34,7 @@ class ProbEnumerator(val filename: String, val vocab: VocabFactory, val oeManage
   //var fos = new FileOutputStream("check.txt", true)
   var phaseCounter : Int = 0
   var fitsMap = mutable.Map[(Class[_], Option[Any]), Double]()
-  var costLevel = 10
+  var costLevel = 1
   ProbUpdate.probMap = ProbUpdate.createProbMap(task.vocab)
   ProbUpdate.priors = ProbUpdate.createPrior(task.vocab)
   var timeout = 3 * probMap((classOf[StringReplace], None))
