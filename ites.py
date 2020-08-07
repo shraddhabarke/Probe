1df153fe326613dcd0b808998f96ab751881131b
Shraddha Barke <shraddha.6596@gmail.com>
2020-08-07 12:19:18 -0700

    plots

---
+++ ites.py
@@ -0,0 +1,81 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.lines as mlines
+import matplotlib.transforms as mtransforms
+from collections import defaultdict
+
+ite_probe = defaultdict(list)
+ite_temp_probe = defaultdict(list)
+ite_cvc4 = defaultdict(list)
+examples = defaultdict(list)
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+with open('results/probe.csv', 'r') as f:
+    data = f.readlines()
+    if data[0] != "Benchmark,Program,Time,Size,Ite\n":
+        line_prepender("results/probe.csv", "Benchmark,Program,Time,Size,Ite\n")  
+
+with open('results/cvc4.csv', 'r') as f:
+    data = f.readlines()
+    if data[0] != "Benchmark,Examples,Program,Time,Ite\n":
+        line_prepender("results/cvc4.csv", "Benchmark,Examples,Program,Time,Ite\n") 
+
+with open('results/probe.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader:
+        for (k,v) in row.items(): 
+            if (v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                ite_temp_probe[k].append(v)
+
+def process_benchmark(col, cols):
+    process = list(map(lambda x: x, cols[col]))
+    return process
+
+with open('results/cvc4.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader: 
+        for (k,v) in row.items(): 
+            if (row['Benchmark'] in process_benchmark('Benchmark', ite_temp_probe) and v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                ite_cvc4[k].append(v)
+
+with open('results/probe.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader:
+        for (k,v) in row.items(): 
+            if (row['Benchmark'] in process_benchmark('Benchmark', ite_cvc4) and v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                ite_probe[k].append(v)
+
+def process_ite(col, cols):
+    process = list(map(lambda x: x, cols[col]))
+    process_int = list(map(lambda x: int(x), process))
+    return process_int
+
+cvc4 = process_ite('Ite',ite_cvc4)
+probe = process_ite('Ite',ite_cvc4)
+examples = process_ite('Examples',ite_cvc4)
+cvc4zip = list(zip(cvc4,examples))
+probezip = list(zip(probe,examples))
+cvc4ite = list(map(lambda x: x[0]/x[1], cvc4zip))
+probeite = list(map(lambda x: x[0]/x[1], probezip))
+
+fig, ax = plt.subplots(figsize=(7, 6))
+
+ax.scatter(cvc4ite, probeite, c='blue', marker='o')
+lims = [
+    np.min([-0.01, 1]),  # min of both axes
+    np.max([0, 1]),  # max of both axes
+]
+#ax.add_line(line)
+ax.plot(lims, lims, 'k-', color='red', alpha=0.75)
+ax.set_xlim(lims)
+ax.set_ylim(lims)
+plt.xlabel('ITEs per example (CVC4)')
+plt.ylabel('ITEs per example (Probe)')
+plt.savefig('figures/ite.png')
---
+++ plot-times-unguided.py
@@ -0,0 +1,88 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+from collections import defaultdict
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+with open('results/probe.csv', 'r') as f:
+    data = f.readlines()
+    print(data[0])
+    if data[0] != "Benchmark,Program,Time,Size,Ite\n":
+        line_prepender("results/probe.csv", "Benchmark,Program,Time,Size,Ite\n")       
+
+with open('results/size.csv', 'r') as f:
+    data = f.readlines()
+    print(data[0])
+    if data[0] != "Benchmark,Program,Time,Size\n":
+        line_prepender("results/size.csv", "Benchmark,Program,Time,Size\n")
+
+with open('results/height.csv', 'r') as f:
+    data = f.readlines()
+    print(data[0])
+    if data[0] != "Benchmark,Program,Time,Size,Ite\n":
+        line_prepender("results/height.csv", "Benchmark,Program,Time,Size\n")
+
+time_height = defaultdict(list)
+time_probe = defaultdict(list)
+time_size = defaultdict(list)
+
+with open('results/size.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader: 
+        for (k,v) in row.items(): 
+            if (v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                time_size[k].append(v)
+
+with open('results/height.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader:
+        for (k,v) in row.items(): 
+            if (v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                time_height[k].append(v)
+
+with open('results/probe.csv') as f:
+    reader = csv.DictReader(f) 
+    for row in reader:
+        for (k,v) in row.items(): 
+            if (v != 'Timeout' and v != 'MemoryError' and v != '' and v != None):
+                time_probe[k].append(v)
+
+
+def process_data(col, cols):
+    process = list(map(lambda x: float(x), cols[col]))
+    process_count = [[x,process.count(x)] for x in set(process)]
+    process_count = sorted(process_count, key=lambda x: x[0])
+    return process_count
+
+size = process_data('Time',time_size)
+height = process_data('Time',time_height)
+probe = process_data('Time',time_probe)
+
+
+print(list(zip([x[0] for x in probe], np.cumsum(np.asarray([x[1] for x in probe])))))
+print(list(zip([x[0] for x in size], np.cumsum(np.asarray([x[1] for x in size])))))
+print(list(zip([x[0] for x in height], np.cumsum(np.asarray([x[1] for x in height])))))
+
+size.append([600,0])
+height.append([600,0])
+probe.append([600,0])
+
+plt.figure(figsize=(15, 7))
+plt.xlim(-4,605)
+plt.ylim(-4,80) 
+plt.plot([x[0] for x in probe], np.cumsum(np.asarray([x[1] for x in probe])), 'b--', linewidth=2, label='Probe', markersize = 4)
+plt.legend(loc='best')
+plt.plot([x[0] for x in size], np.cumsum(np.asarray([x[1] for x in size])), 'g--', linewidth=2, label='Size-based', markersize = 4)
+plt.legend(loc='best')
+plt.plot([x[0] for x in height], np.cumsum(np.asarray([x[1] for x in height])), 'r--', linewidth=2, label='Height-based', markersize = 4)
+plt.legend(loc='best')
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Number of benchmarks solved')
+plt.savefig('figures/probe-vs-unguided.png')
