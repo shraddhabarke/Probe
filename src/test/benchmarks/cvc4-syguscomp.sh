a139aec662e0e38972d0fa1d893cfe024c4f5973
Shraddha Barke <shraddha.6596@gmail.com>
2020-08-06 19:00:57 -0700

    cvc4 script decimal times

--- src/test/benchmarks/cvc4-syguscomp.sh
+++ src/test/benchmarks/cvc4-syguscomp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for f in euphony/*.sl; do
+for f in string/*.sl; do
 	STARTTIME=$(date +%s%N)
-	out=`timeout 600 /c/users/shrad/downloads/cvc4-1.7-win64-opt.exe $f`
+	out=`timeout 600 cvc4 --lang=sygus1 $f`
 	ENDTIME=$(date +%s%N)
 	ret=$(echo $out | tr '\r' '\n' | awk 'END{print}')
 	strret=( $ret )
-	echo ${#strret[@]},$((($ENDTIME - $STARTTIME)/1000000000)),$ret
+	ret=${ret//"unsat"/}
+	diff=$(echo "scale=3; ($ENDTIME - $STARTTIME)/1000000000" | bc -l)   
+	echo $f, $ret, ${#strret[@]},$diff
 done
