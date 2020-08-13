9db498d3886ed3632d0b50c42cd737de4129604a
Shraddha Barke <shraddha.6596@gmail.com>
2020-08-12 18:02:44 -0700

    sygus to smt2, cex model

--- src/main/scala/SMTProcess.scala
+++ src/main/scala/SMTProcess.scala
@@ -3,6 +3,8 @@ import sygus.{SyGuSLexer, SyGuSParser}
 
 import collection.JavaConverters._
 import ast._
+
+import scala.collection.mutable.ListBuffer
 import scala.sys.process._
 
 object SMTProcess {
@@ -20,10 +22,46 @@ object SMTProcess {
       out = scala.io.Source.fromInputStream(istream).getLines().toList
     }
     val cvc4 = cmd.run(io)
-    print(cvc4)
     if(cvc4.exitValue() != 0) Nil
     else if (out.head == "unknown") Nil //unsynthesizable
     else out
 
+  }
+
+  def toSMT(syData: String): String = {
+    val parsed = new SyGuSParser(new BufferedTokenStream(new SyGuSLexer(CharStreams.fromString(syData)))).syGuS()
+    val synthFun = parsed.cmd().asScala.filter(cmd => cmd.getChild(1) != null && cmd.getChild(1).getText == "synth-fun").head
+    val defineFun = parsed.cmd().asScala.filter(cmd => cmd.smtCmd() != null).map(_.smtCmd()).filter(cmd => cmd.getChild(1).getText == "define-fun").head
+    val functionName = synthFun.Symbol(0).getSymbol.getText
+    val functionReturnType = Types.withName(synthFun.sort().getText.replaceAllLiterally("(","").replaceAllLiterally(")",""))
+    val functionParameters = synthFun.sortedVar().asScala.map(svar =>
+      (svar.Symbol().getText -> svar.sort().getText.replaceAllLiterally("(","").replaceAllLiterally(")","").replaceAll("64", " 64"))).toList
+    val functionParametersString = functionParameters.map{v => v._1}.mkString(" ")
+    val targetTerm = for (i <- 0 to 5 if defineFun.term().term(i) != null)
+      yield {defineFun.term().term(i).getText}
+    val lhs = '(' + defineFun.term().identifier().getText + targetTerm.mkString("") + ')'
+    val lhsFormat = functionParameters.foldLeft(lhs)((curr, str) => curr.replaceAll(s"(?<![#v])${str._1}", s" ${str._1}"))
+      .replaceAll(s"(?<![\\(])\\(", s" \\(").replaceAll(s"\\)(?![\\)])", s"\\) ")
+      .replaceAll("  +", " ").replaceAll("\\s+$", "").replaceAll("^\\s+", "")
+    val rhs = '(' + functionName + " " + functionParametersString + ')'
+
+    val smtString = "(set-option :produce-models true)\n" +
+      "(set-logic ALL)\n" +
+        functionParameters.map{v => s"(declare-const ${v._1} (_ ${v._2}))"}.mkString("", "\n", "\n") +
+        s"(declare-fun $functionName" + " (" + functionParameters.map(v => s"(_ ${v._2})").mkString(" ") + ") " + s"(_ ${functionReturnType.toString.replaceAll("64", " 64")}))\n" +
+        s"(assert (not (= $lhsFormat $rhs)))\n" +
+        "(check-sat)\n" +
+        s"(get-value ($functionParametersString))"
+
+    smtString
+  }
+
+  def checkSat(solverOut: List[String]): Boolean = if (solverOut.head == "sat") true else false
+
+  def getCEx(query: List[String], solverOut: List[String]) : ListBuffer[Map[String, Any]] = {
+    var modelMap: ListBuffer[Map[String, Any]] = ListBuffer()
+    val words = solverOut.last.split("\\) \\(").toList.map(c => c.substring(c.indexOf("bv") + 2, c.indexOf("64")).trim)
+    modelMap += (query zip words).toMap
+    modelMap
   }
 }
--- src/test/scala/CVCTests.scala
+++ src/test/scala/CVCTests.scala
@@ -1,9 +1,37 @@
-import org.junit.Assert.assertEquals
-import org.junit.Test
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import sygus.SygusFileTask
+import org.junit.Assert._
 
 class CVCTests extends FunSuite with BeforeAndAfterAll {
+
+  test("Parsing sygus to SMTlib") {
+    val synthConvert = SMTProcess.toSMT(
+      """(set-logic BV)
+        |
+        |(define-fun hd14 ((x (BitVec 64)) (y (BitVec 64))) (BitVec 64) (bvadd (bvand x y) (bvlshr (bvxor x y) #x0000000000000001)))
+        |
+        |(synth-fun f ((x (BitVec 64)) (y (BitVec 64))) (BitVec 64)
+        |    ((Start (BitVec 64) ((bvlshr Start Start)
+        |						 (bvxor Start Start)
+        |						 (bvadd Start Start)
+        |						 (bvand Start Start)
+        |						 x
+        |						 y
+        |						 #x0000000000000001))))
+        |
+        |(declare-var x (BitVec 64))
+        |(declare-var y (BitVec 64))
+        |(constraint (= (hd14 x y) (f x y)))
+        |(check-synth)""".stripMargin)
+    val synthRes = SMTProcess.invokeCVC(synthConvert.stripMargin, SMTProcess.cvc4_Smt)
+    assert(synthRes == List("sat", "((x (_ bv18446744073709551615 64)) (y (_ bv18446744073709551615 64)))"))
+  }
+
+  test("Getting cex from query output") {
+    val synthRes = SMTProcess.getCEx(List("x", "y"),
+      List("sat", "((x (_ bv18446744073709551615 64)) (y (_ bv18446744073709551615 64)))"))
+    assert(synthRes == List(Map("x" -> "18446744073709551615", "y" -> "18446744073709551615")))
+  }
 
   test("Parsing SMT2 format LIA") {
     val synthRes = SMTProcess.invokeCVC(
